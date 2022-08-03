0ef6eca7f0a79daf6465df85bf20131bde953c8f
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-03 15:08:20 +0200

    chore: added dataset factory from scandeval with new names

--- src/aiai_eval/task_factory.py
+++ src/aiai_eval/task_factory.py
@@ -1,1 +1,21 @@
 """Factory that produces tasks from a task configuration."""
+
+from typing import Type, Union
+
+from .config import EvaluationConfig
+
+
+class TaskFactory:
+    """Factory which produces tasks from a configuration.
+    Args:
+        evaluation_config (EvaluationConfig):
+            The benchmark configuration to be used in all tasks constructed.
+    Attributes:
+        evaluation_config (EvaluationConfig):
+            The benchmark configuration to be used in all tasks constructed.
+    """
+
+    def __init__(self, evaluation_config: EvaluationConfig):
+        self.evaluation_config = evaluation_config
+
+    # TODO build_dataset(DatasetConfig)
