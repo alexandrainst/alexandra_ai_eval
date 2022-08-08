30aeb33f98fe9a3037c45693d4d95af6483646cd
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-08 14:37:49 +0200

    feat: added InvalidEvaluation exception

--- src/aiai_eval/exceptions.py
+++ src/aiai_eval/exceptions.py
@@ -1,1 +1,9 @@
 """Custom exceptions used in the project."""
+
+
+class InvalidEvaluation(Exception):
+    def __init__(
+        self, message: str = "This model cannot be evaluated on the given dataset."
+    ):
+        self.message = message
+        super().__init__(self.message)
