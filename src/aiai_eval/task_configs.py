44d55ef7ab88a4599cc02468419f19c33cbd36b8
Anders Jess Pedersen <38854604+AJDERS@users.noreply.github.com>
2022-08-09 12:43:02 +0200

    Update src/aiai_eval/task_configs.py, dataset pretty name
    Co-authored-by: Dan Saattrup Nielsen <47701536+saattrupdan@users.noreply.github.com>

--- src/aiai_eval/task_configs.py
+++ src/aiai_eval/task_configs.py
@@ -18,7 +18,7 @@ def get_all_dataset_tasks() -> Dict[str, DatasetTask]:
 NER = DatasetTask(
     name="ner",
     dataset_name="dane",
-    pretty_dataset_name="The Danish Dependency Treebank",
+    pretty_dataset_name="DaNE",
     huggingface_id="dane",
     supertask="token-classification",
     metrics=[
