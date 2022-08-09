53b58926e889aaad0b72cfa11d09d91f1c153ac0
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 14:31:18 +0200

    Added abstract evaluation class

--- src/aiai_eval/task.py
+++ src/aiai_eval/task.py
@@ -1,1 +1,37 @@
 """Abstract Task class."""
+
+from abc import ABC
+
+from datasets import load_metric
+
+from .config import DatasetTask, EvaluationConfig
+
+
+class EvaluationDataset(ABC):
+    """Abstract evaluation dataset class.
+    Args:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    Attributes:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    """
+
+    def __init__(self, dataset_task: DatasetTask, evaluation_config: EvaluationConfig):
+        """Initialise the dataset.
+        Args:
+            dataset_config (DatasetConfig):
+                The configuration for the dataset.
+            evaluation_config (BenchmarkConfig):
+                The configuration for the benchmark.
+        """
+        self.dataset_task = dataset_task
+        self.evaluation_config = evaluation_config
+        self._metrics = {
+            metric_cfg.name: load_metric(metric_cfg.huggingface_id)
+            for metric_cfg in dataset_task.metrics
+        }
