e3306bbe4f4e2f94bbef5cc508aa57fcc2dd6f4f
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 12:41:52 +0200

    Added missing docstring

--- src/aiai_eval/hf_hub.py
+++ src/aiai_eval/hf_hub.py
@@ -4,6 +4,14 @@ from huggingface_hub.utils import RepositoryNotFoundError
 
 
 def model_exists_on_hf_hub(model_id: str) -> bool:
+    """Function checks if `model_id` exists on Huggingface Hub.
+
+    Args:
+        model_id (str): The model ID to check.
+
+    Returns:
+        bool: If model exists on Hugginface Hub or not.
+    """
     hf_api = HfApi()
     try:
         hf_api.model_info(model_id)
