980627cf979e170b9e9df8321fdf6d80c22325fc
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 15:01:28 +0200

    Added evaluate placeholder function and call function

--- src/aiai_eval/task.py
+++ src/aiai_eval/task.py
@@ -37,5 +37,19 @@ class EvaluationDataset(ABC):
             for metric_cfg in dataset_task.metrics
         }
 
-    def benchmark(self, model_id: str) -> Dict[str, dict]:
+    def evaluate(self, model_id: str) -> Dict[str, dict]:
+        """Evaluate a model.
+        Args:
+            model_id (str):
+                The full Hugging Face Hub path to the pretrained transformer model. The
+                specific model version to use can be added after the suffix '@':
+                "model_id@v1.0.0". It can be a branch name, a tag name, or a commit id.
+        Returns:
+            dict:
+                The keys in the dict are 'raw' and 'total', with all the raw scores in
+                the first dictionary and the aggregated scores in the second.
+        """
         return {model_id: {"foo": "bar"}}
+
+    def __call__(self, *args, **kwargs):
+        return self.benchmark(*args, **kwargs)
