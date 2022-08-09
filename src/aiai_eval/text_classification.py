a227f7b997bed8e094d1957e5cf0aee3724c4483
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 14:38:09 +0200

    Added classes for text classification

--- src/aiai_eval/text_classification.py
+++ src/aiai_eval/text_classification.py
@@ -1,1 +1,47 @@
 """Class for text classification tasks."""
+from .task import DatasetTask
+
+
+class TextClassificationEvaluation(DatasetTask):
+    """Text classification benchmark dataset.
+    Args:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    Attributes:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    """
+
+
+class SentimentAnalysis(TextClassificationEvaluation):
+    """Sentiment analysis evaluation dataset.
+    Args:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    Attributes:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    """
+
+
+class OffensiveSpeechClassification(TextClassificationEvaluation):
+    """Sentiment analysis evaluation dataset.
+    Args:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    Attributes:
+        dataset_task (DatasetTask):
+            The configuration of the dataset task.
+        evaluation_config (EvaluationConfig):
+            The configuration of the evaluation.
+    """
