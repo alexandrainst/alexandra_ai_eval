c002aa1a6a579aaabd528a56360d1fd3cde8e3e5
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 15:52:23 +0200

    fix: formatting, and reference to class

--- src/aiai_eval/text_classification.py
+++ src/aiai_eval/text_classification.py
@@ -1,14 +1,16 @@
 """Class for text classification tasks."""
-from .task import EvaluationDataset
+from .task import EvaluationTask
 
 
-class SentimentAnalysis(EvaluationDataset):
-    """Sentiment analysis evaluation dataset.
+class SentimentAnalysis(EvaluationTask):
+    """Sentiment analysis evaluation task.
+
     Args:
         dataset_task (DatasetTask):
             The configuration of the dataset task.
         evaluation_config (EvaluationConfig):
             The configuration of the evaluation.
+
     Attributes:
         dataset_task (DatasetTask):
             The configuration of the dataset task.
@@ -17,13 +19,15 @@ class SentimentAnalysis(EvaluationDataset):
     """
 
 
-class OffensiveSpeechClassification(EvaluationDataset):
-    """Offensive Speech classification evaluation dataset.
+class OffensiveSpeechClassification(EvaluationTask):
+    """Offensive Speech classification evaluation task.
+
     Args:
         dataset_task (DatasetTask):
             The configuration of the dataset task.
         evaluation_config (EvaluationConfig):
             The configuration of the evaluation.
+
     Attributes:
         dataset_task (DatasetTask):
             The configuration of the dataset task.
