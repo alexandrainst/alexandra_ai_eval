fc2ac46f81e0898c102cc185fa759966551abd68
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-09 14:38:26 +0200

    Added classes for NER

--- src/aiai_eval/named_entity_recognition.py
+++ src/aiai_eval/named_entity_recognition.py
@@ -1,1 +1,33 @@
 """Class for named entity recognition tasks."""
+
+from .task import DatasetTask
+
+
+class TokenClassificationEvaluation(DatasetTask):
+    """Token classification benchmark dataset.
+    Args:
+        dataset_config (DatasetConfig):
+            The dataset configuration.
+        benchmark_config (BenchmarkConfig):
+            The benchmark configuration.
+    Attributes:
+        dataset_config (DatasetConfig):
+            The configuration of the dataset.
+        benchmark_config (BenchmarkConfig):
+            The configuration of the benchmark.
+    """
+
+
+class NEREvaluation(TokenClassificationEvaluation):
+    """Token classification benchmark dataset.
+    Args:
+        dataset_config (DatasetConfig):
+            The dataset configuration.
+        benchmark_config (BenchmarkConfig):
+            The benchmark configuration.
+    Attributes:
+        dataset_config (DatasetConfig):
+            The configuration of the dataset.
+        benchmark_config (BenchmarkConfig):
+            The configuration of the benchmark.
+    """
