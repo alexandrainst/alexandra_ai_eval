109a64c6e03ac5df1ccb1076a3c545a175bb5d66
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-10 10:51:49 +0200

    feat: Added new exceptions

--- src/aiai_eval/exceptions.py
+++ src/aiai_eval/exceptions.py
@@ -17,3 +17,22 @@ class ModelDoesNotExistOnHuggingFaceHubException(Exception):
         self.model_id = model_id
         self.message = f"The model {model_id} does not exist on the Hugging Face Hub"
         super().__init__(self.message)
+
+
+class InvalidFramework(Exception):
+    def __init__(self, framework: str):
+        self.framework = framework
+        self.message = f"The framework {framework} is not supported."
+        super().__init__(self.message)
+
+
+class HuggingFaceHubDown(Exception):
+    def __init__(self, message: str = "The Hugging Face Hub is currently down."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NoInternetConnection(Exception):
+    def __init__(self, message: str = "There is currently no internet connection."):
+        self.message = message
+        super().__init__(self.message)
