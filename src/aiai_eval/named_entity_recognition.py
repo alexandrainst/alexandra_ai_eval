a6e5b1c1d03950c67cc2a242814f5a37af30cf2f
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-08-15 14:16:20 +0200

    chore: docstrings

--- src/aiai_eval/named_entity_recognition.py
+++ src/aiai_eval/named_entity_recognition.py
@@ -23,12 +23,16 @@ class NEREvaluation(EvaluationTask):
 
     def _preprocess_data(self, dataset: Dataset, framework: str, **kwargs) -> Dataset:
         """Preprocess a dataset by tokenizing and aligning the labels.
+
         Args:
             dataset (Hugging Face dataset):
                 The dataset to preprocess.
+            framework (str):
+                Specification of which framework the model is created in.
             kwargs:
                 Extra keyword arguments containing objects used in preprocessing the
                 dataset.
+
         Returns:
             Hugging Face dataset: The preprocessed dataset.
         """
