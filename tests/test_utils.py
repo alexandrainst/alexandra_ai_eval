a38d717d62a671cdd231ee69b03e083e0558b764
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-16 15:12:45 +0200

    tests: Add unit tests for the utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,24 @@
 """Unit tests for the `utils` module."""
+
+from src.aiai_eval.utils import (
+    block_terminal_output,
+    clear_memory,
+    enforce_reproducibility,
+    is_module_installed,
+)
+
+
+class TestClearMemory:
+    pass
+
+
+class TestEnforceReproducibility:
+    pass
+
+
+class TestIsModuleInstalled:
+    pass
+
+
+class TestBlockTerminalOutput:
+    pass
