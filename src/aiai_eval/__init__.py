a80d3b4a0d01831a64938d74dea0e1a519f995da
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-19 11:04:22 +0200

    feat: Set up __init__

--- src/aiai_eval/__init__.py
+++ src/aiai_eval/__init__.py
@@ -2,7 +2,34 @@
 .. include:: ../../README.md
 """
 
+import logging
+import os
+
+import colorama
 import pkg_resources
+from termcolor import colored
+
+from .evaluator import Evaluator  # noqa
+from .utils import block_terminal_output
 
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = pkg_resources.get_distribution("aiai_eval").version
+
+
+# Block unwanted terminal outputs
+block_terminal_output()
+
+
+# Ensure that termcolor also works on Windows
+colorama.init()
+
+
+# Set up logging
+fmt = colored("%(asctime)s [%(levelname)s] <%(name)s>\n↳ ", "cyan") + colored(
+    "%(message)s", "yellow"
+)
+logging.basicConfig(level=logging.INFO, format=fmt)
+
+
+# Disable parallelisation when tokenizing, as that can lead to errors
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
