9aed4a6b4cc7865f4e153353f1ff40c234dc695d
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-23 16:11:12 +0200

    tests: Add global `task_config` fixture, change test cache dir to '.aiai_cache'

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,19 +3,28 @@
 import pytest
 
 from src.aiai_eval.config import EvaluationConfig
+from src.aiai_eval.task_configs import get_all_task_configs
+from src.aiai_eval.utils import Device
 
 
 @pytest.fixture(scope="session")
 def evaluation_config():
     yield EvaluationConfig(
         raise_error_on_invalid_model=True,
-        cache_dir="cache_dir",
+        cache_dir=".aiai_cache",
         use_auth_token=False,
         progress_bar=False,
         save_results=True,
         verbose=True,
         track_carbon_emissions=True,
         country_iso_code="DNK",
-        prefer_device="cpu",
+        prefer_device=Device.CPU,
         testing=True,
     )
+
+
+@pytest.fixture(
+    scope="session", params=get_all_task_configs().values(), ids=lambda cfg: cfg.name
+)
+def task_config(request):
+    yield request.param
