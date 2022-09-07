fd05f24610bedae7cbfc5638812ea68b94a26240
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-09-07 17:13:13 +0200

    docs: Remove docstrings from abstract methods in NamedEntityRecognition

--- src/aiai_eval/named_entity_recognition.py
+++ src/aiai_eval/named_entity_recognition.py
@@ -165,21 +165,7 @@ class NamedEntityRecognition(Task):
     """
 
     def _preprocess_data(self, dataset: Dataset, framework: str, **kwargs) -> Dataset:
-        """Preprocess the data.
 
-        Args:
-            dataset (Hugging Face dataset):
-                The dataset to preprocess.
-            framework (str):
-                Specification of which framework the model is created in.
-            kwargs:
-                Extra keyword arguments containing objects used in preprocessing the
-                dataset.
-
-        Returns:
-            Hugging Face dataset:
-                The preprocessed dataset.
-        """
         if framework == "spacy":
             raise InvalidEvaluation(
                 "Evaluation of named entity recognition for SpaCy models is not yet "
@@ -200,145 +186,10 @@ class NamedEntityRecognition(Task):
 
         return tokenised_dataset
 
-    def _tokenize_and_align_labels(
-        self, examples: dict, tokenizer, label2id: dict
-    ) -> dict:
-        """Tokenise all texts and align the labels with them.
-
-        Args:
-            examples (dict):
-                The examples to be tokenised.
-            tokenizer (Hugging Face tokenizer):
-                A pretrained tokenizer.
-            label2id (dict):
-                A dictionary that converts NER tags to IDs.
-
-        Returns:
-            dict:
-                A dictionary containing the tokenized data as well as labels.
-        """
-
-        tokenized_inputs = tokenizer(
-            examples["tokens"],
-            is_split_into_words=True,
-            truncation=True,
-            padding=True,
-        )
-        all_labels: List[List[int]] = []
-        for i, ner_tags in enumerate(examples[self.task_config.label_column_name]):
-            labels = [self.task_config.id2label[ner_tag] for ner_tag in ner_tags]
-            try:
-                word_ids = tokenized_inputs.word_ids(batch_index=i)
-
-            # This happens if the tokenizer is not of the fast variant, in which case
-            # the `word_ids` method is not available, so we have to extract this
-            # manually. It's slower, but it works, and it should only occur rarely,
-            # when the Hugging Face team has not implemented a fast variant of the
-            # tokenizer yet.
-            except ValueError:
-
-                # Get the list of words in the document
-                words = examples["tokens"][i]
-
-                # Get the list of token IDs in the document
-                tok_ids = tokenized_inputs.input_ids[i]
-
-                # Decode the token IDs
-                tokens = tokenizer.convert_ids_to_tokens(tok_ids)
-
-                # Remove prefixes from the tokens
-                prefixes_to_remove = ["▁", "##"]
-                for tok_idx, tok in enumerate(tokens):
-                    for prefix in prefixes_to_remove:
-                        tok = tok.lstrip(prefix)
-                    tokens[tok_idx] = tok
-
-                # Replace special tokens with `None`
-                sp_toks = tokenizer.special_tokens_map.values()
-                tokens = [None if tok in sp_toks else tok for tok in tokens]
-
-                # Get the alignment between the words and the tokens, on a character
-                # level
-                word_idxs = [
-                    word_idx for word_idx, word in enumerate(words) for _ in str(word)
-                ]
-                token_idxs = [
-                    tok_idx
-                    for tok_idx, tok in enumerate(tokens)
-                    for _ in str(tok)
-                    if tok is not None
-                ]
-                alignment = list(zip(word_idxs, token_idxs))
-
-                # Raise error if there are not as many characters in the words as in
-                # the tokens. This can be due to the use of a different prefix.
-                if len(word_idxs) != len(token_idxs):
-                    tokenizer_type = type(tokenizer).__name__
-                    raise InvalidTokenizer(
-                        tokenizer_type=tokenizer_type,
-                        message=(
-                            "The tokens could not be aligned with the words during "
-                            "manual word-token alignment. It seems that the tokenizer "
-                            "is neither of the fast variant nor of a SentencePiece/"
-                            "WordPiece variant. The tokenizer type is "
-                            f"{tokenizer_type}."
-                        ),
-                    )
-
-                # Get the aligned word IDs
-                word_ids = list()
-                for tok_idx, tok in enumerate(tokens):
-                    if tok is None or tok == "":
-                        word_ids.append(None)
-                    else:
-                        word_idx = [
-                            word_idx
-                            for word_idx, token_idx in alignment
-                            if token_idx == tok_idx
-                        ][0]
-                        word_ids.append(word_idx)
-
-            previous_word_idx = None
-            label_ids: List[int] = []
-            for word_idx in word_ids:
-
-                # Special tokens have a word id that is None. We set the label to -100
-                # so they are automatically ignored in the loss function
-                if word_idx is None:
-                    label_ids.append(-100)
-
-                # We set the label for the first token of each word
-                elif word_idx != previous_word_idx:
-                    label = labels[word_idx]
-                    try:
-                        label_id = label2id[label.upper()]
-                    except KeyError:
-                        raise MissingLabel(label=label, label2id=label2id)
-                    label_ids.append(label_id)
-
-                # For the other tokens in a word, we set the label to -100
-                else:
-                    label_ids.append(-100)
-
-                previous_word_idx = word_idx
-
-            all_labels.append(label_ids)
-        tokenized_inputs["labels"] = all_labels
-        return tokenized_inputs
-
     def _load_data_collator(
         self, tokenizer: PreTrainedTokenizerBase
     ) -> DataCollatorForTokenClassification:
-        """Load the data collator used to prepare samples during finetuning.
 
-        Args:
-            tokenizer (Hugging Face tokenizer):
-                A pretrained tokenizer.
-
-        Returns:
-            Hugging Face data collator:
-                The data collator.
-        """
         return DataCollatorForTokenClassification(tokenizer, label_pad_token_id=-100)
 
     def _prepare_predictions_and_labels(
@@ -348,24 +199,7 @@ class NamedEntityRecognition(Task):
         prepared_dataset: Dataset,
         **kwargs,
     ) -> List[Tuple[list, list]]:
-        """Prepare predictions and labels for output.
 
-        Args:
-            predictions (sequence of either ints or floats):
-                The predictions of the model, which can be either class labels or
-                logits.
-            dataset (Dataset):
-                The raw dataset.
-            prepared_dataset (Dataset):
-                The prepared dataset.
-            kwargs:
-                Extra keyword arguments containing objects used in preparing the
-                predictions and labels.
-
-        Returns:
-            list of pairs of lists:
-                The prepared predictions and labels.
-        """
         # Extract the `model_id2label` mapping
         model_id2label = kwargs["model_id2label"]
 
