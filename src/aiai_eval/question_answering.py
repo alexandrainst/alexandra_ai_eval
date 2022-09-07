b370aefc657f11e964f88f5d1e650ad08564ecb4
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-09-07 17:16:03 +0200

    chore: Remove unused method from QuestionAnswering

--- src/aiai_eval/question_answering.py
+++ src/aiai_eval/question_answering.py
@@ -267,22 +267,3 @@ class QuestionAnswering(Task):
             labels.append(label)
 
         return [(predictions, labels)]
-
-    def _get_spacy_predictions_and_labels(self, model, dataset: Dataset) -> tuple:
-        """Get predictions from SpaCy model on dataset.
-
-        Args:
-            model (SpaCy model):
-                The model.
-            dataset (Hugging Face dataset):
-                The dataset.
-
-        Returns:
-            A pair of arrays:
-                The first array contains the probability predictions and the second
-                array contains the true labels.
-        """
-        raise InvalidEvaluation(
-            "Evaluation of question answering tasks for SpaCy models is currently "
-            "not possible."
-        )
