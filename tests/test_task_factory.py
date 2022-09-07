ad29f95764e05cad8183d7087cd87d4450dbab91
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-09-07 18:01:12 +0200

    tests: Fix the task_factory isinstance test

--- tests/test_task_factory.py
+++ tests/test_task_factory.py
@@ -3,10 +3,8 @@
 import pytest
 
 from src.aiai_eval.config import Label
-from src.aiai_eval.named_entity_recognition import NamedEntityRecognition
-from src.aiai_eval.question_answering import QuestionAnswering
-from src.aiai_eval.sequence_classification import SequenceClassification
 from src.aiai_eval.task_factory import TaskFactory
+from src.aiai_eval.utils import kebab_to_pascal
 
 
 @pytest.fixture(scope="module")
@@ -31,9 +29,7 @@ def test_configs_are_preserved(task_config, task_factory, evaluation_config):
 
 def test_build_task(task_config, task_factory):
     task = task_factory.build_task(task_name_or_config=task_config)
-    if task_config.supertask == "sequence-classification":
-        assert isinstance(task, SequenceClassification)
-    elif task_config.name == "ner":
-        assert isinstance(task, NamedEntityRecognition)
-    elif task_config.name == "qa":
-        assert isinstance(task, QuestionAnswering)
+    assert type(task).__name__ in [
+        kebab_to_pascal(task_config.name),
+        kebab_to_pascal(task_config.supertask),
+    ]
