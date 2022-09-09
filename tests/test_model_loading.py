e131c7dfd251339018436a518eebddd9f43da159
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-09-09 19:19:15 +0200

    tests: Deal with spaCy models in model loading test

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 
 import pytest
 import torch
+from spacy.language import Language
 
 from src.aiai_eval.exceptions import InvalidFramework
 from src.aiai_eval.model_loading import load_model
@@ -16,7 +17,10 @@ def test_load_model(model_configs, task_config, evaluation_config):
             task_config=task_config,
             evaluation_config=evaluation_config,
         )["model"]
-        assert isinstance(model, torch.nn.Module)
+        if model_config.framework == "pytorch":
+            assert isinstance(model, torch.nn.Module)
+        elif model_config.framework == "spacy":
+            assert isinstance(model, Language)
 
 
 def test_invalid_model_framework(model_configs, task_config, evaluation_config):
