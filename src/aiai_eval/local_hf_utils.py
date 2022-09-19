158f6e1ef7589d9f3ac9a52a082d532a09ab2c52
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-09-19 12:53:11 +0200

    feat: Better existence test of local HF models

--- src/aiai_eval/local_hf_utils.py
+++ src/aiai_eval/local_hf_utils.py
@@ -96,11 +96,29 @@ def hf_model_exists_locally(model_id: Union[str, Path]) -> bool:
         bool:
             Whether the model exists locally.
     """
+    # Ensure that `model_id` is a Path object
+    model_id = Path(model_id)
+
+    # Return False if the model folder does not exist
+    if not model_id.exists():
+        return False
+
+    # Try to load the model config. If this fails, False is returned
     try:
-        AutoConfig.from_pretrained(model_id)
-        return True
+        AutoConfig.from_pretrained(str(model_id))
     except OSError:
         return False
+
+    # Check that a compatible model file exists
+    pytorch_model_exists = model_id.glob("*.bin") or model_id.glob("*.pt")
+    jax_model_exists = model_id.glob("*.msgpack")
+
+    # If no model file exists, return False
+    if not pytorch_model_exists and not jax_model_exists:
+        return False
+
+    # Otherwise, if all these checks succeeded, return True
+    return True
 
 
 def get_hf_model_config_locally(model_folder: Union[str, Path]) -> ModelConfig:
