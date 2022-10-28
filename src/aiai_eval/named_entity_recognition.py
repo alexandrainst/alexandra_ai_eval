f3ab68f7fca863ef09f03e638f09734386b742da
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2022-10-28 11:52:44 +0200

    fix: small logic error with checking of model predictions

--- src/aiai_eval/named_entity_recognition.py
+++ src/aiai_eval/named_entity_recognition.py
@@ -128,8 +128,10 @@ class NamedEntityRecognition(Task):
 
         sample_preds = model_predictions[0]
         has_sequence_elements = len(sample_preds[0]) > 0
-        leaves_are_floats = sample_preds[0][0].dtype.kind == "f"
         elements_are_strings = isinstance(sample_preds[0], str)
+        leaves_are_floats = False
+        if not elements_are_strings:
+            leaves_are_floats = sample_preds[0][0].dtype.kind == "f"
 
         return (has_sequence_elements and leaves_are_floats) or elements_are_strings
 
