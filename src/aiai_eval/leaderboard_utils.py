42ceefb5abad4e14e18a1acef09e6c63d9132f19
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2023-01-12 10:26:55 +0100

    fix: formatting
    Signed-off-by: Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>

--- src/aiai_eval/leaderboard_utils.py
+++ src/aiai_eval/leaderboard_utils.py
@@ -7,7 +7,6 @@ from urllib3.util.retry import Retry
 from .task_configs import get_all_task_configs
 
 
-
 class Session(requests.Session):
     """A requests session that automatically adds the API key to the headers."""
 
@@ -16,8 +15,8 @@ class Session(requests.Session):
         self.base_url = base_url
         retry = Retry(connect=3, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
-        self.mount('http://', adapter)
-        self.mount('https://', adapter)
+        self.mount("http://", adapter)
+        self.mount("https://", adapter)
 
     def get_task(self, task_name: str, raw: bool = False) -> dict:
         """Get the leaderboard for the task corresponding to task_name.
@@ -38,7 +37,6 @@ class Session(requests.Session):
         except KeyError:
             raise ValueError(f"Task {task_name} not found.")
 
-        
         # Create endpoint, taking into account if we are getting raw data or not
         if raw:
             endpoint = f"{self.base_url}/{task_config.name}-raw"
@@ -53,7 +51,9 @@ class Session(requests.Session):
             raise ValueError(f"Task {task_name} not found.")
         return task
 
-    def get_model_for_task(self, task_name: str, model_id: str, raw: bool = False) -> dict:
+    def get_model_for_task(
+        self, task_name: str, model_id: str, raw: bool = False
+    ) -> dict:
         """Get the entries on leaderboard for the model_id for the task corresponding to task_name.
 
         Args:
@@ -90,15 +90,17 @@ class Session(requests.Session):
             raise ValueError(f"Model {model_id} not found.")
         return task
 
-    def post_model_to_task(self, model_type: str, task_name: str, model_id: str, metrics: dict) -> dict:
+    def post_model_to_task(
+        self, model_type: str, task_name: str, model_id: str, metrics: dict
+    ) -> dict:
         """Post a model to the leaderboard for the task corresponding to task_name.
-        
+
         Args:
             model_type (str): The model type.
             task_name (str): The name of the task.
             model_id (str): The model id.
             metrics (dict): A dictionary with the metrics for the model.
-            
+
         Returns:
             dict: A dictionary with the possibly updated leaderboard.
         """
